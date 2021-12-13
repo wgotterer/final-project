ad43834bfbee2410beee23d0f3a5a1e4743a8f4c
William Gotterer <william.gotterer@gmail.com>
2021-12-13 13:06:29 -0500

    made keyboard

--- client/src/components/Key.js
+++ client/src/components/Key.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
  function Key({note}) {
+
+    
+
+    // const noteIsFlat = (note) => {
+    //     return note.length > 1;
+    //   }
+
+      console.log(note.length)
+
     return (
-        <div className="key">
-            <div className="key-text">
-                {note.toUpperCase()}
-            </div>
+        
+        <div >
+        {note.length > 1 ? <div className="key_flat"> {note.toUpperCase()}</div> : <div className="key">{note.toUpperCase()}</div> }
+            
             
         </div>
     )
