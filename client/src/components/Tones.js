118ec0b17b17d6297cbce4f59390f2b0f1132056
William Gotterer <william.gotterer@gmail.com>
2021-12-23 07:27:15 -0800

    new scales

---
+++ client/src/components/Tones.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import * as Tone from "tone";
+  
+
+
+export function playA3() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("A3", "8n");
+  }
+ export function playBb3() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("Bb3", "8n");
+  }
+export function playB3() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("B3", "8n");
+  }
+export function playC4() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("C4", "8n");
+
+  }
+ export function playDb4() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("Db4", "8n");
+  }
+ export function playD4() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("D4", "8n");
+  }
+ export function playEb4() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("Eb4", "8n");
+  }
+ export function playE4() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("E4", "8n");
+  }
+ export function playF4() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("F4", "8n");
+  }
+ export function playGb4() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("Gb4", "8n");
+  }
+ export function playG4() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("G4", "8n");
+  }
+ export function playAb4() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("Ab4", "8n");
+  }
+ export function playA4() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("A4", "8n");
+  }
+ export function playBb4() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("Bb4", "8n");
+  }
+ export function playB4() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("B4", "8n");
+  }
+ export function playC5() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("C5", "8n");
+  }
+  export function playDb5() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("Db5", "8n");
+  }
+  export function playD5() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("D5", "8n");
+  }
+  export function playEb5() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("Eb5", "8n");
+  }
+  export function playE5() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("E5", "8n");
+  }
+  export function playF5() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("F5", "8n");
+  }
+  export function playGb5() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("Gb5", "8n");
+  }
